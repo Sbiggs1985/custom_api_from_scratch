20ba5839dcea588e93a4c41d1745efa3066266d0
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-12-07 16:43:27 -0600

    update

--- models/index.js
+++ models/index.js
@@ -10,5 +10,4 @@ RecentlyPlayedSong.belongsTo(User, { foreignKey: 'userId' });
 Song.hasMany(RecentlyPlayedSong, { foreignKey: 'songId' });
 RecentlyPlayedSong.belongsTo(Song, { foreignKey: 'songId' });
 
-// Export models and Sequelize instance
 module.exports = { sequelize, User, Song, RecentlyPlayedSong };
