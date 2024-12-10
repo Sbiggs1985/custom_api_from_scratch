84c5e33fcc3e8d9f010f8290a385a5e0fb36e1f3
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-12-10 15:02:00 -0600

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,18 +1,8 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Load environment variables
-
-console.log('Database Config:', {
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_NAME: process.env.database,
-  DB_PASSWORD: process.env.root,
-});
-
 // Initialize Sequelize
 const sequelize = new Sequelize(
-  process.env.database,
-  process.env.postgre, // Username
-  process.env.password, // Password
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST, // ????
     port: process.env.DB_PORT,
